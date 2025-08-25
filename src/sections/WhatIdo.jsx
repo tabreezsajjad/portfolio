5ae61a1884fa8a874f6ebba5e20f3549976ef1b5
Sajjad <tabreezsajjad@yahoo.com>
2025-08-25 17:56:05 +0530

    fixed name casing

--- src/sections/WhatIdo.jsx
+++ src/sections/WhatIdo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import "./WhatIDo.css";
+import "./WhatIdo.css";
 
 export default function WhatIDo() {
   return (
